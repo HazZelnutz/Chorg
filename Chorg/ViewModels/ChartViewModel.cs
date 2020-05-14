1294846153813dc922f074126567ab6a03aa875a
HazZelnutz <darrellparker10@gmail.com>
2020-05-14 09:13:37 +0200

    Created ContentType Converters

--- Chorg/ViewModels/ChartViewModel.cs
+++ Chorg/ViewModels/ChartViewModel.cs
@@ -32,82 +32,9 @@ namespace Chorg.ViewModels
             get => model.Content;
         }
 
-        public string ContentString
-        {
-            get 
-            {
-                switch (model.Content)
-                {
-                    case ContentType.GENERAL:
-                        return "GEN";
-                    case ContentType.TAXI:
-                        return "TAXI";
-                    case ContentType.SID:
-                        return "SID";
-                    case ContentType.STAR:
-                        return "STAR";
-                    case ContentType.APP:
-                        return "APP";
-                    case ContentType.UNDEFINED:
-                        return "N.D.";
-                    default:
-                        throw new Exception();
-                }
-            }
-        }
-
-        public SolidColorBrush Accent 
-        {
-            get {
-                switch (model.Content)
-                {
-                    case ContentType.GENERAL:
-                        return (SolidColorBrush)App.Current.FindResource("Color_GEN");
-                    case ContentType.TAXI:
-                        return (SolidColorBrush)App.Current.FindResource("Color_TAXI");
-                    case ContentType.SID:
-                        return (SolidColorBrush)App.Current.FindResource("Color_SID");
-                    case ContentType.STAR:
-                        return (SolidColorBrush)App.Current.FindResource("Color_STAR");
-                    case ContentType.APP:
-                        return (SolidColorBrush)App.Current.FindResource("Color_APP");
-                    case ContentType.UNDEFINED:
-                        return (SolidColorBrush)App.Current.FindResource("Color_ND");
-                    default:
-                        throw new Exception();
-                }
-            }
-        }
-
         public ChartViewModel(Chart model)
         {
             this.model = model;
-            this.model.PropertyChanged += ModelChanged;
-        }
-
-        /// <summary>
-        /// Informs the view when the model changes (e.g. because of edit)
-        /// </summary>
-        private void ModelChanged(object sender, PropertyChangedEventArgs e)
-        {
-            switch (e.PropertyName)
-            {
-                case "Identifier":
-                    NotifyOfPropertyChange(() => Identifier);
-                    break;
-
-                case "Description":
-                    NotifyOfPropertyChange(() => Description);
-                    break;
-
-                case "Content":
-                    NotifyOfPropertyChange(() => Accent);
-                    NotifyOfPropertyChange(() => ContentString);
-                    break;
-
-                default:
-                    break;
-            }
         }
 
         public Chart GetModel()
